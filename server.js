21f52544f5f8f5b235cff55f0499efeb296fbd08
arpitnath <arpitnath42@gmail.com>
2020-10-03 05:08:31 +0530

    release0

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+
+const app = express();
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose
+  .connect("MONGOURI", { useNewUrlParser: true })
+  .then(() => console.log("MongoDB successfully connected!"))
+  .catch((err) => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
